a01095011611268f61bb2f5e862237c11cc67ec9
npquanh <npquanh30402@gmail.com>
2024-06-11 15:26:59 +0700

    Implement global chat WebSocket

--- src/webSocket.ts
+++ src/webSocket.ts
@@ -1,7 +1,14 @@
 import { Elysia } from "elysia";
 import { UserType } from "./database/schemas/userTable";
 import { authJwt } from "./configs";
+import { db } from "./database/db";
+import { eq } from "drizzle-orm";
+import { profileTable } from "./database/schemas";
 import { checkAuthenticatedMiddleware } from "./middleware";
+import { server } from "./index";
+
+let users: any[] = [];
+let messages: any[] = [];
 
 export const webSocketRoute = new Elysia({
   prefix: "/ws",
@@ -18,7 +25,20 @@ export const webSocketRoute = new Elysia({
     },
   })
   .resolve(async ({ cookie: { auth }, authJwt }) => {
-    const authUser = (await authJwt.verify(auth.value)) as unknown as UserType;
+    const user = (await authJwt.verify(auth.value)) as unknown as UserType;
+
+    const profile = await db.query.profileTable.findFirst({
+      columns: {
+        avatarUrl: true,
+      },
+      where: eq(profileTable.userId, user.userId),
+    });
+
+    const authUser = {
+      ...user,
+      avatarUrl: profile?.avatarUrl,
+    };
+
     return {
       authUser,
     };
@@ -31,5 +51,70 @@ export const webSocketRoute = new Elysia({
       ws.subscribe(`private-notification-${authUser.userId}`);
       ws.subscribe(`private-conversation-${authUser.userId}`);
       ws.subscribe(`private-friend-${authUser.userId}`);
+    },
+  })
+  .ws("/global-chat", {
+    open(ws) {
+      const { authUser } = ws.data;
+      ws.subscribe("global-chat");
+
+      const user = {
+        userId: authUser.userId,
+        username: authUser.username,
+        avatarUrl: authUser.avatarUrl,
+      };
+
+      users.push(user);
+
+      ws.publish(
+        "global-chat",
+        JSON.stringify({ type: "USERS_ADD", data: user }),
+      );
+
+      ws.send(JSON.stringify({ type: "USERS_SET", data: users }));
+      ws.send(JSON.stringify({ type: "MESSAGES_SET", data: messages }));
+    },
+    message(ws, message) {
+      const { authUser } = ws.data;
+
+      const user = {
+        userId: authUser.userId,
+        username: authUser.username,
+        avatarUrl: authUser.avatarUrl,
+      };
+
+      const msg = {
+        message,
+        createdAt: new Date(),
+      };
+
+      const send = {
+        ...user,
+        ...msg,
+      };
+
+      messages.push(send);
+
+      ws.publish(
+        "global-chat",
+        JSON.stringify({ type: "MESSAGE_ADD", data: send }),
+      );
+
+      ws.send(JSON.stringify({ type: "MESSAGE_ADD", data: send }));
+    },
+    close(ws) {
+      const { authUser } = ws.data;
+
+      users = users.filter((user) => user.userId !== authUser.userId);
+
+      if (users.length === 0) {
+        messages = [];
+        return;
+      }
+
+      server?.publish(
+        "global-chat",
+        JSON.stringify({ type: "USER_REMOVE", data: authUser }),
+      );
     },
   });
